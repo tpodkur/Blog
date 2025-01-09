75e569d6e3e41a1d22d9f0d1d84d2f7984a40a55
Tatiana <t.podkur17@gmail.com>
2025-01-10 00:04:09 +0700

    add Dto for server data

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,13 +1,35 @@
 import axios from 'axios';
+import { z } from 'zod';
 
 const baseURL = 'https://blog-platform.kata.academy/api';
+
+const ArticleDto = z.object({
+  articles: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      description: z.string(),
+      tagList: z.array(z.string()),
+      body: z.string(),
+      author: z.object({
+        username: z.string(),
+        image: z.string(),
+        following: z.boolean(),
+      }),
+      createdAt: z.string(),
+      favorited: z.boolean(),
+      favoritesCount: z.number(),
+    })
+  ),
+  articlesCount: z.number(),
+});
 
 export const api = {
   getPublications: async () => {
     return await axios
       .get(`${baseURL}/articles`)
       .then((response) => {
-        console.log(response);
+        return ArticleDto.parse(response.data);
       })
       .catch(console.log);
   },
