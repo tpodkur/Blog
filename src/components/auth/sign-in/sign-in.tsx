69587ec555810e70062a97b5ff4abbf169287fcd
Tatiana <t.podkur17@gmail.com>
2025-01-15 23:53:16 +0700

    fix bug: swap sign-in and sign-up content

--- src/components/auth/sign-in/sign-in.tsx
+++ src/components/auth/sign-in/sign-in.tsx
@@ -5,11 +5,7 @@ import classes from '../auth.module.scss';
 const SignIn = () => {
   return (
     <form method="post" className={classes.auth}>
-      <h1 className={classes.auth__title}>Create new account</h1>
-      <label className={classes.auth__label}>
-        Username
-        <input type="text" name="username" placeholder="Username" className={classes.auth__input} />
-      </label>
+      <h1 className={classes.auth__title}>Sign In</h1>
       <label className={classes.auth__label}>
         Email address
         <input type="email" name="email" placeholder="Email address" className={classes.auth__input} />
@@ -18,26 +14,11 @@ const SignIn = () => {
         Password
         <input type="password" name="password" placeholder="Password" className={classes.auth__input} />
       </label>
-      <label className={classes.auth__label}>
-        Repeat password
-        <input type="password" name="repeat-password" placeholder="Repeat password" className={classes.auth__input} />
-      </label>
-      <label className={`${classes.auth__permission} ${classes.permission}`}>
-        <input
-          type="checkbox"
-          name="agree-checkbox"
-          id="agree-checkbox"
-          className={classes.permission__input}
-          checked
-        />
-        <span className={classes.permission__checkbox}></span>
-        <span className={classes.permission__text}>I agree to the processing of my personal information</span>
-      </label>
-      <input type="submit" value="Create" className={classes.auth__submit} />
+      <input type="submit" value="Login" className={classes.auth__submit} />
       <p className={classes.auth__message}>
-        Already have an account?{' '}
+        Don&#39;t have an account?{' '}
         <Link to="/sign-up" className={classes.auth__link}>
-          Sign In.
+          Sign Up.
         </Link>
       </p>
     </form>
--- src/components/auth/sign-up/sign-up.tsx
+++ src/components/auth/sign-up/sign-up.tsx
@@ -5,7 +5,11 @@ import classes from '../auth.module.scss';
 const SignUp = () => {
   return (
     <form method="post" className={classes.auth}>
-      <h1 className={classes.auth__title}>Sign In</h1>
+      <h1 className={classes.auth__title}>Create new account</h1>
+      <label className={classes.auth__label}>
+        Username
+        <input type="text" name="username" placeholder="Username" className={classes.auth__input} />
+      </label>
       <label className={classes.auth__label}>
         Email address
         <input type="email" name="email" placeholder="Email address" className={classes.auth__input} />
@@ -14,11 +18,26 @@ const SignUp = () => {
         Password
         <input type="password" name="password" placeholder="Password" className={classes.auth__input} />
       </label>
-      <input type="submit" value="Login" className={classes.auth__submit} />
+      <label className={classes.auth__label}>
+        Repeat password
+        <input type="password" name="repeat-password" placeholder="Repeat password" className={classes.auth__input} />
+      </label>
+      <label className={`${classes.auth__permission} ${classes.permission}`}>
+        <input
+          type="checkbox"
+          name="agree-checkbox"
+          id="agree-checkbox"
+          className={classes.permission__input}
+          checked
+        />
+        <span className={classes.permission__checkbox}></span>
+        <span className={classes.permission__text}>I agree to the processing of my personal information</span>
+      </label>
+      <input type="submit" value="Create" className={classes.auth__submit} />
       <p className={classes.auth__message}>
-        Don&#39;t have an account?{' '}
+        Already have an account?{' '}
         <Link to="/sign-in" className={classes.auth__link}>
-          Sign Up.
+          Sign In.
         </Link>
       </p>
     </form>
