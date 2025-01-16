e64810ccdb2a0fe33bb86ca0e3c1bca7e3e087ae
Tatiana <t.podkur17@gmail.com>
2025-01-16 21:14:34 +0700

    add validation for Sign In form

--- src/components/auth/sign-in/sign-in.tsx
+++ src/components/auth/sign-in/sign-in.tsx
@@ -1,20 +1,47 @@
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z, ZodType } from 'zod';
 
 import classes from '../auth.module.scss';
 
+type FormValues = {
+  email: string;
+  password: string;
+};
+
+const SignInSchema: ZodType<FormValues> = z.object({
+  email: z.string().min(1, { message: 'This is required' }).email({ message: 'Must be a valid email' }),
+  password: z.string().min(1, { message: 'This is required' }),
+});
+
 const SignIn = () => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm<FormValues>({ resolver: zodResolver(SignInSchema), mode: 'onBlur' });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    reset();
+  });
+
   return (
-    <form method="post" className={classes.auth}>
+    <form method="post" onSubmit={onSubmit} className={classes.auth}>
       <h1 className={classes.auth__title}>Sign In</h1>
       <label className={classes.auth__label}>
         Email address
-        <input type="email" name="email" placeholder="Email address" className={classes.auth__input} />
+        <input type="email" placeholder="Email address" className={classes.auth__input} {...register('email')} />
+        {errors.email && <p>{errors?.email?.message}</p>}
       </label>
       <label className={classes.auth__label}>
         Password
-        <input type="password" name="password" placeholder="Password" className={classes.auth__input} />
+        <input type="password" placeholder="Password" className={classes.auth__input} {...register('password')} />
       </label>
-      <input type="submit" value="Login" className={classes.auth__submit} />
+      {errors.password && <p>{errors?.password?.message}</p>}
+      <input type="submit" value="Login" className={classes.auth__submit} disabled={!isValid} />
       <p className={classes.auth__message}>
         Don&#39;t have an account?{' '}
         <Link to="/sign-up" className={classes.auth__link}>
