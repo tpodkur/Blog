ec25917086c862695625f5febfa5e46b814f1dea
Tatiana <t.podkur17@gmail.com>
2025-01-22 20:23:39 +0700

    add tags inputs

--- src/components/forms/create-article/create-article.tsx
+++ src/components/forms/create-article/create-article.tsx
@@ -1,16 +1,28 @@
-import { useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z, ZodType } from 'zod';
+import { Button } from 'antd';
 
 import classes from '../form.module.scss';
+
+type FormValues = {
+  title: string;
+  description: string;
+  text: string;
+  tags: {
+    value: string;
+  }[];
+};
 
 const CreateArticleSchema: ZodType<FormValues> = z.object({
   title: z.string(),
   description: z.string(),
   text: z.string(),
-  tags: z.array(z.string()),
-  email: z.string().min(1, { message: 'This is required.' }).email({ message: 'Must be a valid email.' }),
-  password: z.string().min(1, { message: 'This is required.' }),
+  tags: z.array(
+    z.object({
+      value: z.string(),
+    })
+  ),
 });
 
 const CreateArticle = () => {
@@ -19,11 +31,38 @@ const CreateArticle = () => {
     formState: { errors },
     handleSubmit,
     reset,
-  } = useForm<FormValues>({ resolver: zodResolver(CreateArticleSchema), mode: 'onSubmit' });
+    control,
+  } = useForm<FormValues>({
+    resolver: zodResolver(CreateArticleSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      tags: [{ value: '' }, { value: '' }],
+    },
+  });
+  const { fields } = useFieldArray({
+    name: 'tags',
+    control,
+  });
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
     reset();
+  });
+
+  const tagsList = fields.map((field, index) => {
+    return (
+      <div key={field.id} className={classes.tag}>
+        <input
+          type="text"
+          placeholder="Tag"
+          className={`${classes.input} ${classes.tag__input} ${errors.tag && classes['input--not-valid']}`}
+          {...register(`tags.${index}.value`)}
+        />
+        <Button danger className={`${classes['tag-button']}`}>
+          Delete
+        </Button>
+      </div>
+    );
   });
 
   return (
@@ -34,7 +73,7 @@ const CreateArticle = () => {
         <input
           type="text"
           placeholder="Title"
-          className={`${classes.form__input} ${errors.title && classes['form__input--not-valid']}`}
+          className={`${classes.input} ${errors.title && classes['input--not-valid']}`}
           {...register('title')}
         />
         {errors.title && <p className={classes['form__validation-message']}>{errors?.title?.message}</p>}
@@ -44,7 +83,7 @@ const CreateArticle = () => {
         <input
           type="text"
           placeholder="Short description"
-          className={`${classes.form__input} ${errors.description && classes['form__input--not-valid']}`}
+          className={`${classes.input} ${errors.description && classes['input--not-valid']}`}
           {...register('description')}
         />
         {errors.description && <p className={classes['form__validation-message']}>{errors?.description?.message}</p>}
@@ -52,11 +91,25 @@ const CreateArticle = () => {
       <label className={`${classes.form__label} ${classes['form__label--size_M']}`}>
         Text
         <textarea
-          className={`${classes.form__input} ${classes['form__input--textarea']} ${errors.text && classes['form__input--not-valid']}`}
+          className={`${classes.input} ${classes['input--textarea']} ${errors.text && classes['input--not-valid']}`}
           placeholder="Text"
           {...register('text')}
         ></textarea>
         {errors.text && <p className={classes['form__validation-message']}>{errors?.text?.message}</p>}
+      </label>
+      <label className={`${classes.form__label} ${classes['form__label--size_M']}`}>
+        Tags
+        <div className={classes['tags-list']}>
+          <div className={classes['tags-list__inputs']}>{tagsList}</div>
+          <Button
+            color="primary"
+            variant="outlined"
+            className={`${classes['tags-list__add']} ${classes['tag-button']}`}
+          >
+            Add tag
+          </Button>
+        </div>
+        {errors.tag && <p className={classes['form__validation-message']}>{errors?.tag?.message}</p>}
       </label>
       <input type="submit" value="Send" className={`${classes.form__submit} ${classes['form__submit--size_S']}`} />
     </form>
